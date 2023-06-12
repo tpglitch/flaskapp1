6a3ad6523f6045799e44594b9267cefbcc8de182
Tyler Kinney/Teagon <123958345+tpglitch@users.noreply.github.com>
2023-06-12 11:11:31 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app['SECRET_KEY'] = '123savesum4me'
+
+@app.route('/')
+def home():
+  return jsonify({"Message":"Teating"})
+
+if __name__ == '__main__':
+  app.run()
